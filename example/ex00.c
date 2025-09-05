9c218038b59d7771975182af09173005744e18df
Vapankung <88593427+Vapankung@users.noreply.github.com>
2025-09-05 02:16:33 +0000

    wr

---
+++ example/ex00.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num, count;
+    int range1, range2, range3, range4 = 0;
+    float high[300], sumhigh = 0, avg = 0;
+
+    printf("Enter n");
+
+    for(count = 0; count<num; count++){
+
+    }
+}
